576f7748d7e50c9d63fa635068782122ef28c1de
Abarna-453 <abarnask2003@gmail.com>
2024-04-04 20:00:50 +0530

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // esbuild: {
+  //   loader: "jsx", // Specify "jsx" as the loader for JSX syntax in JS files
+  //   include: [
+  //     // Add the specific files and directories where you want to allow JSX syntax in JS files
+  //     "src/**/*.js",
+  //     "node_modules/**/*.js",
+  //   ],
+  // },
+  esbuild: {
+    jsxFactory: 'React.createElement', // Specify the JSX factory function
+    jsxFragment: 'React.Fragment', // Specify the JSX fragment component
+  },
+});
