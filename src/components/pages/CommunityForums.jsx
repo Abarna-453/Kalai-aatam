12b530a4f1797e175acb338228c5454e485810fb
avanthika12 <avanthikaa1207@gmail.com>
2024-04-16 15:44:56 +0530

    integration

--- src/components/pages/CommunityForums.jsx
+++ src/components/pages/CommunityForums.jsx
@@ -1,6 +1,14 @@
-import React from "react";
 
+import React, { useEffect } from "react";
 
-export const CommunityForums= () => {
-  return <h1>CommunityForums</h1>;
+export const CommunityForums = () => {
+  useEffect(() => {
+    // Redirect to http://localhost:3000/
+    window.open("http://localhost:3000");
+    window.location.href = "http://localhost:5173/";
+  }, []); // Empty dependency array ensures useEffect runs only once after component mounts
+
+  return <div>{/* Your Navbar content goes here */}</div>;
 };
+
+
